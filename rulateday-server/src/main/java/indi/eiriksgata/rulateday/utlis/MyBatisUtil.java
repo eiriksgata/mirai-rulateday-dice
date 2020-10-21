c0ee1fbe678d970207a98f253ba14261ee409416
Keith404 <2353686862@qq.com>
2020-10-21 10:51:39 +0800

    add Mybatis config file.

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/MyBatisUtil.java
@@ -0,0 +1,43 @@
+package indi.eiriksgata.rulateday.utlis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class MyBatisUtil {
+
+    public static SqlSessionFactory factory = createSFactory();
+    public static final ThreadLocal<SqlSession> THREAD_LOCAL = new ThreadLocal<>();
+
+    private static SqlSessionFactory createSFactory() {
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            return builder.build(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static SqlSession getSqlSession() {
+        if (THREAD_LOCAL.get() == null) {
+            THREAD_LOCAL.set(factory.openSession());
+        }
+        return THREAD_LOCAL.get();
+    }
+
+    public static void closeSqlSession() {
+        if (THREAD_LOCAL.get() != null) {
+            THREAD_LOCAL.get().close();
+            THREAD_LOCAL.remove();
+        }
+    }
+}
+
+
