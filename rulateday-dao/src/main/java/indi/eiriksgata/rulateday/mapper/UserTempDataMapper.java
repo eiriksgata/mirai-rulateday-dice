d7576d089b618b092eacb9211effeaf4f6a02ca5
Keith404 <2353686862@qq.com>
2020-10-23 09:45:42 +0800

    add 'insert' and 'updateAttributeById' mapper function.

--- rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/UserTempDataMapper.java
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/UserTempDataMapper.java
@@ -1,9 +1,7 @@
 package indi.eiriksgata.rulateday.mapper;
 
 import indi.eiriksgata.rulateday.pojo.UserTempData;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 /**
  * @author: create by Keith
@@ -18,5 +16,12 @@ public interface UserTempDataMapper {
     @Select("select * from user_temp_data where id=#{id}")
     UserTempData selectById(@Param("id") Long id);
 
+    @Insert("insert into user_temp_data (id,attribute,dice_face) values (#{id},#{attribute},#{dice_face})")
+    void insert(UserTempData userTempData);
 
+    @Update("update user_temp_data set attribute = #{attribute} where id =#{id}")
+    void updateAttributeById(@Param("id") Long id, @Param("attribute") String attribute);
+
+    @Update("update user_temp_data set dice_face = #{diceFace} where id=#{id}")
+    void updateDiceFaceById(@Param("id") Long id, @Param("diceFace") int diceFace);
 }
