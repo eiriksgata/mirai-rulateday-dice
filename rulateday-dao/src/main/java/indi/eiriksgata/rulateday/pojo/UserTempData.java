88ae10fa2d7ae9a11eae6f129e42e60c283d2415
Keith404 <2353686862@qq.com>
2020-10-26 09:39:39 +0800

    update use 'lombok.Data'.

--- rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/UserTempData.java
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/UserTempData.java
@@ -1,37 +1,12 @@
 package indi.eiriksgata.rulateday.pojo;
 
+import lombok.Data;
 
+@Data
 public class UserTempData {
 
     private long id;
     private String attribute;
-    private int diceFace;
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-
-    public String getAttribute() {
-        return attribute;
-    }
-
-    public void setAttribute(String attribute) {
-        this.attribute = attribute;
-    }
-
-
-    public int getDiceFace() {
-        return diceFace;
-    }
-
-    public void setDiceFace(int diceFace) {
-        this.diceFace = diceFace;
-    }
+    private int dice_face;
 
 }
