1748cad90131eb2a999a355d80292d22c516f91c
Keith404 <2353686862@qq.com>
2020-10-28 11:18:40 +0800

    update accept handle group message.

--- rulateday-server/src/main/java/indi/eiriksgata/rulateday/DiceMessageEventHandle.java
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/DiceMessageEventHandle.java
@@ -1,22 +1,22 @@
 package indi.eiriksgata.rulateday;
 
-import kotlin.Unit;
+import indi.eiriksgata.dice.exception.DiceInstructException;
+import indi.eiriksgata.dice.exception.ExceptionEnum;
+import indi.eiriksgata.dice.message.handle.InstructHandle;
+import indi.eiriksgata.dice.vo.MessageData;
+import indi.eiriksgata.rulateday.mapper.UserTempDataMapper;
+import indi.eiriksgata.rulateday.utlis.MyBatisUtil;
 import kotlin.coroutines.CoroutineContext;
-import kotlinx.coroutines.Job;
-import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.ListeningStatus;
 import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.message.FriendMessageEvent;
 import net.mamoe.mirai.message.GroupMessageEvent;
-import net.mamoe.mirai.message.MessageReceipt;
 import net.mamoe.mirai.message.data.*;
+import net.mamoe.mirai.utils.MiraiLogger;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * @author: create by Keith
@@ -24,8 +24,9 @@ import java.util.concurrent.Future;
  * @description: indi.eiriksgata.rulateday
  * @date:2020/10/15
  **/
-
 public class DiceMessageEventHandle extends SimpleListenerHost {
+
+    public static InstructHandle instructHandle = new InstructHandle();
 
     @EventHandler()
     public ListeningStatus onFriendMessage(FriendMessageEvent event) {
@@ -44,82 +45,107 @@ public class DiceMessageEventHandle extends SimpleListenerHost {
     public ListeningStatus onGroupMessage(GroupMessageEvent event) {
         //群消息的回复
 
-        String msgString = event.getMessage().toString();
-
-        if (msgString.contains(".rh")) {
-            event.getSender().sendMessage("暂无该功能");
+        MessageData messageData = new MessageData();
+        messageData.setMessage(event.getMessage().contentToString());
+        messageData.setQqID(event.getSender().getId());
+        String result;
+        try {
+            result = instructHandle.instructCheck(messageData);
+        } catch (DiceInstructException e) {
+            if (e.getErrCode().equals(ExceptionEnum.DICE_INSTRUCT_NOT_FOUND.getErrCode())) {
+                return ListeningStatus.LISTENING;
+            }
+            result = e.getErrMsg();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            result = "非法访问异常";
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+            result = "实例化异常";
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+            result = "反射架构异常";
         }
 
-        if (msgString.contains("reply")) {
-            // 引用回复
-            final QuoteReply quote = new QuoteReply(event.getSource());
-            event.getGroup().sendMessage(quote.plus("引用回复"));
+        final QuoteReply quoteReply = new QuoteReply(event.getSource());
+        event.getGroup().sendMessage(quoteReply.plus(result));
 
-        } else if (msgString.contains("at")) {
-            // at
-            event.getGroup().sendMessage(new At(event.getSender()));
+//        if (msgString.contains(".rh")) {
+//            event.getSender().sendMessage("暂无该功能");
+//        }
+//
+//        if (msgString.contains("reply")) {
+//            // 引用回复
+//            final QuoteReply quote = new QuoteReply(event.getSource());
+//            event.getGroup().sendMessage(quote.plus("引用回复"));
+//
+//        } else if (msgString.contains("at")) {
+//            // at
+//            event.getGroup().sendMessage(new At(event.getSender()));
+//
+//        } else if (msgString.contains("permission")) {
+//            // 成员权限
+//            event.getGroup().sendMessage(event.getPermission().toString());
+//
+//
+//        } else if (msgString.contains("mixed")) {
+//            // 复合消息, 通过 .plus 连接两个消息
+//            event.getGroup().sendMessage(
+//                    MessageUtils.newImage("{01E9451B-70ED-EAE3-B37C-101F1EEBF5B5}.png") // 演示图片, 可能已过期
+//                            .plus("Hello") // 文本消息
+//                            .plus(new At(event.getSender())) // at 群成员
+//                            .plus(AtAll.INSTANCE) // at 全体成员
+//            );
+//        } else if (msgString.contains("recall1")) {
+//            event.getGroup().sendMessage("你看不到这条消息").recall();
+//            // 发送消息马上就撤回. 因速度太快, 客户端将看不到这个消息.
+//
+//        } else if (msgString.contains("recall2")) {
+//            final Job job = event.getGroup().sendMessage("3秒后撤回").recallIn(3000);
+//
+//            job.cancel(new CancellationException()); // 可取消这个任务
+//
+//        } else if (msgString.contains("上传图片")) {
+//            File file = new File("myImage.jpg");
+//            if (file.exists()) {
+//                final Image image = event.getGroup().uploadImage(new File("myImage.jpg"));
+//                // 上传一个图片并得到 Image 类型的 Message
+//
+//                final String imageId = image.getImageId(); // 可以拿到 ID
+//                final Image fromId = MessageUtils.newImage(imageId); // ID 转换得到 Image
+//
+//                event.getGroup().sendMessage(image); // 发送图片
+//            }
+//
+//        } else if (msgString.contains("friend")) {
+//            final Future<MessageReceipt<Contact>> future = event.getSender().sendMessageAsync("Async send"); // 异步发送
+//            try {
+//                future.get();
+//            } catch (InterruptedException | ExecutionException e) {
+//                e.printStackTrace();
+//            }
+//            //将图片转换为图片ID
+//        } else if (msgString.startsWith("convert")) {
+//            StringBuilder stringBuilder = new StringBuilder("结果：\n");
+//            event.getMessage().forEachContent(msg ->
+//                    {
+//                        if (msg instanceof Image) {
+//                            stringBuilder.append(((Image) msg).getImageId());
+//                            stringBuilder.append("\n");
+//                        }
+//                        return Unit.INSTANCE;// kotlin 的所有函数都有返回值. Unit 为最基本的返回值. 请在这里永远返回 Unit
+//                    }
+//            );
+//            event.getGroup().sendMessage(stringBuilder.toString());
+//        } else if (msgString.equals("mute")) {
+//            //禁言
+//            event.getSender().mute(100);
+//        } else if (msgString.equals("muteAll")) {
+//            //全体禁言
+//            event.getGroup().getSettings().setMuteAll(true);
+//        }
 
-        } else if (msgString.contains("permission")) {
-            // 成员权限
-            event.getGroup().sendMessage(event.getPermission().toString());
 
-
-        } else if (msgString.contains("mixed")) {
-            // 复合消息, 通过 .plus 连接两个消息
-            event.getGroup().sendMessage(
-                    MessageUtils.newImage("{01E9451B-70ED-EAE3-B37C-101F1EEBF5B5}.png") // 演示图片, 可能已过期
-                            .plus("Hello") // 文本消息
-                            .plus(new At(event.getSender())) // at 群成员
-                            .plus(AtAll.INSTANCE) // at 全体成员
-            );
-        } else if (msgString.contains("recall1")) {
-            event.getGroup().sendMessage("你看不到这条消息").recall();
-            // 发送消息马上就撤回. 因速度太快, 客户端将看不到这个消息.
-
-        } else if (msgString.contains("recall2")) {
-            final Job job = event.getGroup().sendMessage("3秒后撤回").recallIn(3000);
-
-            job.cancel(new CancellationException()); // 可取消这个任务
-
-        } else if (msgString.contains("上传图片")) {
-            File file = new File("myImage.jpg");
-            if (file.exists()) {
-                final Image image = event.getGroup().uploadImage(new File("myImage.jpg"));
-                // 上传一个图片并得到 Image 类型的 Message
-
-                final String imageId = image.getImageId(); // 可以拿到 ID
-                final Image fromId = MessageUtils.newImage(imageId); // ID 转换得到 Image
-
-                event.getGroup().sendMessage(image); // 发送图片
-            }
-
-        } else if (msgString.contains("friend")) {
-            final Future<MessageReceipt<Contact>> future = event.getSender().sendMessageAsync("Async send"); // 异步发送
-            try {
-                future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-            //将图片转换为图片ID
-        } else if (msgString.startsWith("convert")) {
-            StringBuilder stringBuilder = new StringBuilder("结果：\n");
-            event.getMessage().forEachContent(msg ->
-                    {
-                        if (msg instanceof Image) {
-                            stringBuilder.append(((Image) msg).getImageId());
-                            stringBuilder.append("\n");
-                        }
-                        return Unit.INSTANCE;// kotlin 的所有函数都有返回值. Unit 为最基本的返回值. 请在这里永远返回 Unit
-                    }
-            );
-            event.getGroup().sendMessage(stringBuilder.toString());
-        } else if (msgString.equals("mute")) {
-            //禁言
-            event.getSender().mute(100);
-        } else if (msgString.equals("muteAll")) {
-            //全体禁言
-            event.getGroup().getSettings().setMuteAll(true);
-        }
         //保持监听
         return ListeningStatus.LISTENING;
     }
