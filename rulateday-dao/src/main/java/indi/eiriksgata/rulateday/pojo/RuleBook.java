a8dfb5570cdebe39cdf2c548a1117f6d51f22141
Keith404 <2353686862@qq.com>
2020-11-03 16:54:52 +0800

    add rule book pojo.

---
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/RuleBook.java
@@ -0,0 +1,37 @@
+package indi.eiriksgata.rulateday.pojo;
+
+
+public class RuleBook {
+
+  private String id;
+  private String title;
+  private String content;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
