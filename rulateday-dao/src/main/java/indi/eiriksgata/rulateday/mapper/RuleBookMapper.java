729c30dffa0f8b7ed3d489a7919bf421cded94ec
Keith404 <2353686862@qq.com>
2020-11-04 11:32:21 +0800

    add rule book library mapper.

---
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/RuleBookMapper.java
@@ -0,0 +1,22 @@
+package indi.eiriksgata.rulateday.mapper;
+
+import indi.eiriksgata.rulateday.pojo.RuleBook;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.mapper
+ * date: 2020/11/4
+ **/
+
+@Mapper
+public interface RuleBookMapper {
+
+    @Select("select * from rule_book where title like #{title} ")
+    RuleBook selectByTitle(@Param("title") String title);
+
+
+}
