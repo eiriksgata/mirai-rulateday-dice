ab6da37940015e0670ebea090ea42b08b6ae912e
Keith404 <2353686862@qq.com>
2020-11-04 14:06:03 +0800

    add rule book interface.

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/service/RuleService.java
@@ -0,0 +1,13 @@
+package indi.eiriksgata.rulateday.service;
+
+import indi.eiriksgata.rulateday.pojo.RuleBook;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.service
+ * date: 2020/11/4
+ **/
+public interface RuleService {
+    RuleBook selectRule(String title);
+}
