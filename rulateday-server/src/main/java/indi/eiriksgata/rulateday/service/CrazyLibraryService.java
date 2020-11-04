b9cc0ae8d9dafd6d00954614e0c06d65a0b79483
Keith404 <2353686862@qq.com>
2020-11-04 11:33:35 +0800

    add crazy service interface class.

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/service/CrazyLibraryService.java
@@ -0,0 +1,10 @@
+package indi.eiriksgata.rulateday.service;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.service
+ * date: 2020/11/4
+ **/
+public interface CrazyLibraryService {
+}
