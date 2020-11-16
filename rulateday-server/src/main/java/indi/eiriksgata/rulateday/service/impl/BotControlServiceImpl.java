b1332d3725bd6e6905a12d270873d705ca56f2a4
Keith404 <2353686862@qq.com>
2020-11-16 14:35:55 +0800

    add group reply list check.

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/service/impl/BotControlServiceImpl.java
@@ -0,0 +1,41 @@
+package indi.eiriksgata.rulateday.service.impl;
+
+import indi.eiriksgata.rulateday.mapper.SpeakersGroupListMapper;
+import indi.eiriksgata.rulateday.service.BotControlService;
+import indi.eiriksgata.rulateday.utlis.MyBatisUtil;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.service.impl
+ * date: 2020/11/13
+ **/
+public class BotControlServiceImpl implements BotControlService {
+
+    private static SpeakersGroupListMapper speakersGroupListMapper = MyBatisUtil.getSqlSession().getMapper(SpeakersGroupListMapper.class);
+
+    @Override
+    public boolean groupIsEnable(long groupId) {
+        Boolean isEnable = speakersGroupListMapper.selectByGroupId(groupId);
+        return isEnable == null || isEnable;
+    }
+
+    @Override
+    public void groupEnable(long groupId) {
+        if (speakersGroupListMapper.selectByGroupId(groupId) == null) {
+            speakersGroupListMapper.insert(groupId, true);
+        } else {
+            speakersGroupListMapper.updateIsEnableById(groupId, true);
+        }
+    }
+
+    @Override
+    public void groupDisable(long groupId) {
+        if (speakersGroupListMapper.selectByGroupId(groupId) == null) {
+            speakersGroupListMapper.insert(groupId, false);
+        } else {
+            speakersGroupListMapper.updateIsEnableById(groupId, false);
+        }
+    }
+
+}
