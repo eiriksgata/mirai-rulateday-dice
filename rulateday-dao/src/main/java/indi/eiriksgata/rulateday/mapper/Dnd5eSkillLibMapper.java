b5102934384d0740a7bc3c8f29ab70ceb52f916c
Keith404 <2353686862@qq.com>
2020-11-16 14:32:13 +0800

    add dnd5e skill lib mapper.

---
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/Dnd5eSkillLibMapper.java
@@ -0,0 +1,29 @@
+package indi.eiriksgata.rulateday.mapper;
+
+import indi.eiriksgata.rulateday.pojo.Dnd5ESkillLib;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.mapper
+ * date: 2020/11/13
+ **/
+@Mapper
+public interface Dnd5eSkillLibMapper {
+
+    @Select("select * from dnd5e_skill_lib")
+    List<Dnd5ESkillLib> selectAll();
+
+    @Select("select * from dnd5e_skill_lib where name like #{name}")
+    Dnd5ESkillLib selectByName(@Param("name") String name);
+
+
+    @Select("select * from dnd5e_skill_lib where id =#{id}")
+    Dnd5ESkillLib selectById(@Param("id") long id);
+
+}
