edb1dfc37951811eb616c8dc34bd6dcf23efb3e9
Keith404 <2353686862@qq.com>
2020-11-16 15:57:10 +0800

    fix SQLite concurrency problem.

--- rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/MyBatisUtil.java
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/MyBatisUtil.java
@@ -11,7 +11,7 @@ import java.io.Reader;
 public class MyBatisUtil {
 
     private static SqlSessionFactory factory = createSFactory();
-    private static final ThreadLocal<SqlSession> THREAD_LOCAL = new ThreadLocal<>();
+    private static SqlSession sqlSession = factory != null ? factory.openSession() : null;
 
     private static SqlSessionFactory createSFactory() {
         try {
@@ -24,20 +24,10 @@ public class MyBatisUtil {
         return null;
     }
 
-
     public static SqlSession getSqlSession() {
-        if (THREAD_LOCAL.get() == null) {
-            THREAD_LOCAL.set(factory.openSession());
-        }
-        return THREAD_LOCAL.get();
+        return sqlSession;
     }
 
-    public static void closeSqlSession() {
-        if (THREAD_LOCAL.get() != null) {
-            THREAD_LOCAL.get().close();
-            THREAD_LOCAL.remove();
-        }
-    }
 }
 
 
