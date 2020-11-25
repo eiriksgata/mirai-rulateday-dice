ac3aeb3c33463a9029f8e38f739273de94035619
Keith404 <2353686862@qq.com>
2020-11-25 11:01:43 +0800

    add auto create database file function.

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/LoadDatabaseFile.java
@@ -0,0 +1,47 @@
+package indi.eiriksgata.rulateday.utlis;
+
+import indi.eiriksgata.rulateday.RulatedayCore;
+
+import java.io.*;
+
+import static org.apache.ibatis.io.Resources.getResourceAsStream;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.utlis
+ * date: 2020/11/24
+ **/
+public class LoadDatabaseFile {
+
+    public static void init() {
+        try {
+            createDatabaseFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createDatabaseFile() throws IOException {
+        InputStream inputStream = getResourceAsStream("rulateday.db");
+        String path = "data//rulateday";
+        File file = new File(path + "//rulateday.db");
+        File mkdir = new File(path);
+        if (!file.exists()) {
+            if (!mkdir.exists()) {
+                mkdir.mkdirs();
+                RulatedayCore.INSTANCE.getLogger().info("Detecting no database file, creating..");
+            }
+            OutputStream output = new FileOutputStream(file);
+            byte[] bytes = new byte[1];
+            while (inputStream.read(bytes) != -1) {
+                output.write(bytes);
+            }
+            output.flush();
+            inputStream.close();
+            output.close();
+        }
+    }
+
+
+}
