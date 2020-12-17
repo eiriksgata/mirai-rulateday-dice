d74cd5078c41bf9cd5cab2bbcee3a331a9a0e061
Eiriksgata <2353686862@qq.com>
2020-12-17 15:55:15 +0800

    Update import file name

--- rulateday-server/src/main/java/indi/eiriksgata/rulateday/service/impl/Dnd5eLibServiceImpl.java
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/service/impl/Dnd5eLibServiceImpl.java
@@ -1,6 +1,6 @@
 package indi.eiriksgata.rulateday.service.impl;
 
-import indi.eiriksgata.rulateday.mapper.Dnd5eSkillLibMapper;
+import indi.eiriksgata.rulateday.mapper.Dnd5eSkillMapper;
 import indi.eiriksgata.rulateday.pojo.Dnd5ESkillLib;
 import indi.eiriksgata.rulateday.service.Dnd5eLibService;
 import indi.eiriksgata.rulateday.utlis.MyBatisUtil;
@@ -18,20 +18,20 @@ public class Dnd5eLibServiceImpl implements Dnd5eLibService {
 
     @Override
     public List<Dnd5ESkillLib> findAllSkill() {
-        Dnd5eSkillLibMapper skillMapper = MyBatisUtil.getSqlSession().getMapper(Dnd5eSkillLibMapper.class);
+        Dnd5eSkillMapper skillMapper = MyBatisUtil.getSqlSession().getMapper(Dnd5eSkillMapper.class);
         return skillMapper.selectAll();
     }
 
 
     @Override
     public Dnd5ESkillLib findName(String name) {
-        Dnd5eSkillLibMapper skillMapper = MyBatisUtil.getSqlSession().getMapper(Dnd5eSkillLibMapper.class);
+        Dnd5eSkillMapper skillMapper = MyBatisUtil.getSqlSession().getMapper(Dnd5eSkillMapper.class);
         return skillMapper.selectByName("%" + name + "%");
     }
 
     @Override
     public Dnd5ESkillLib findById(long id) {
-        Dnd5eSkillLibMapper skillMapper = MyBatisUtil.getSqlSession().getMapper(Dnd5eSkillLibMapper.class);
+        Dnd5eSkillMapper skillMapper = MyBatisUtil.getSqlSession().getMapper(Dnd5eSkillMapper.class);
         return skillMapper.selectById(id);
     }
 
