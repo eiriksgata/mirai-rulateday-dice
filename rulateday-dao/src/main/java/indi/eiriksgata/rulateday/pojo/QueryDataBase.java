222f9d148471db7442bb63d517a51531e4653cec
Eiriksgata <2353686862@qq.com>
2020-12-17 11:12:44 +0800

    Add query data base pojo

---
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/QueryDataBase.java
@@ -0,0 +1,18 @@
+package indi.eiriksgata.rulateday.pojo;
+
+import lombok.Data;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.pojo
+ * date: 2020/11/26
+ **/
+@Data
+public class QueryDataBase {
+
+    private Long id;
+    private String name;
+    private String describe;
+
+}
