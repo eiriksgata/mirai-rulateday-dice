19ecbf4d24919815cc0b97d64b2722f5aae59444
Eiriksgata <2353686862@qq.com>
2021-01-20 17:39:15 +0800

    Update rulateday project exception handle

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/exception/ExceptionEnum.java
@@ -0,0 +1,58 @@
+package indi.eiriksgata.rulateday.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ExceptionEnum {
+
+    // 系统错误的指令
+    DND5E_MM_NAME_DECODE_ERR(10001, "怪物名称编码出错"),
+
+    // system predefine
+    WARING(4, "Your operation may be abnormal."),
+
+    ATTONITY(3, "No operation content."),
+
+    VERIFIED(2, "Waiting for your verification."),
+
+    SUCCESS(0, "Operation Success."),
+
+    ERROR(-1, "System Error."),
+
+    UNKNOWN(-2, "Unknown Exception.");
+
+    private Integer errCode;
+    private String errMsg;
+
+    /**
+     * @param errCode 系统号(2bit) + 业务模块（2bit）+ 业务异常（2bit）
+     * @param errMsg  异常消息
+     */
+    ExceptionEnum(Integer errCode, String errMsg) {
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    public Integer getErrCode() {
+        return errCode;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    /**
+     * find exception enum by err code
+     *
+     * @param errCode error code
+     * @return exception enum
+     */
+    public static indi.eiriksgata.dice.exception.ExceptionEnum getExceptionEnumByCode(Integer errCode) {
+        for (indi.eiriksgata.dice.exception.ExceptionEnum exceptionEnum : indi.eiriksgata.dice.exception.ExceptionEnum.values()) {
+            if (exceptionEnum.getErrCode().equals(errCode)) {
+                return exceptionEnum;
+            }
+        }
+        return null;
+    }
+}
---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/exception/RulatedayException.java
@@ -0,0 +1,38 @@
+package indi.eiriksgata.rulateday.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+//改成必须捕捉异样并处理，如果不符合指令的全部抛出异常，用于用户处理
+public class RulatedayException extends Exception {
+
+    private static final long serialVersionUID = -6301630219349537830L;
+
+    private Integer errCode;
+    private String errMsg;
+
+    public RulatedayException(Integer errCode, String errMsg) {
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    public RulatedayException(String errMsg) {
+        this.errCode = ExceptionEnum.UNKNOWN.getErrCode();
+        this.errMsg = errMsg;
+    }
+
+    public RulatedayException(ExceptionEnum exceptionEnum) {
+        this.errCode = exceptionEnum.getErrCode();
+        this.errMsg = exceptionEnum.getErrMsg();
+    }
+
+    public RulatedayException(ExceptionEnum exceptionEnum, Throwable cause) {
+        super(cause);
+        this.errCode = exceptionEnum.getErrCode();
+        this.errMsg = exceptionEnum.getErrMsg();
+    }
+
+
+}
