93d418486f43cae402a297290dc5028e9be72da1
Eiriksgata <2353686862@qq.com>
2021-03-12 14:46:26 +0800

    Update UserInitiative pojo

---
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/UserInitiativeData.java
@@ -0,0 +1,14 @@
+package indi.eiriksgata.rulateday.pojo;
+
+import lombok.Data;
+
+@Data
+public class UserInitiativeData {
+
+    private long id;
+    private String name;
+    private String groupId;
+    private long userId;
+    private long value;
+
+}
