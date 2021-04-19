7091a0df48e0ada2d7bd796edf9d70c8f92927d2
Eiriksgata <2353686862@qq.com>
2021-04-19 15:09:29 +0800

    Update card group data mapper

---
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/CardsGroupDataMapper.java
@@ -0,0 +1,15 @@
+package indi.eiriksgata.rulateday.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.mapper
+ * date: 2021/4/19
+ **/
+@Mapper
+public interface CardsGroupDataMapper {
+
+}
---
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/CardsTypeListMapper.java
@@ -0,0 +1,14 @@
+package indi.eiriksgata.rulateday.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.mapper
+ * date: 2021/4/19
+ **/
+@Mapper
+public interface CardsTypeListMapper {
+
+}
