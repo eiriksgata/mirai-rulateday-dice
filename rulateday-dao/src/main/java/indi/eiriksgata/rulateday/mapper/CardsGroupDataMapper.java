938aba755b6e2a1aba87e1037a7992a0675f691a
Eiriksgata <2353686862@qq.com>
2021-05-11 15:47:42 +0800

    Update cards dao mapper function

--- rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/CardsGroupDataMapper.java
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/CardsGroupDataMapper.java
@@ -1,7 +1,9 @@
 package indi.eiriksgata.rulateday.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import indi.eiriksgata.rulateday.pojo.CardsGroupData;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * author: create by Keith
@@ -12,4 +14,20 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface CardsGroupDataMapper {
 
+    @Insert("insert into cards_group_data (id,group_id,type_id,value) values (#{id},#{groupId},#{typeId},#{value})")
+    void insert(CardsGroupData cardsGroupData);
+
+    @Delete("delete from cards_group_data where id = #{id}")
+    void deleteById(@Param("id") Long id);
+
+    @Delete("delete from cards_group_data where group_id = #{groupId}")
+    void clearByGroupId(@Param("groupId") Long groupId);
+
+    @Select("select * from cards_group_data where group_id =#{groupId} ORDER BY RANDOM() LIMIT 1")
+    CardsGroupData randonGetCard(@Param("groupId") Long groupId);
+
+    @Select("select * from cards_group_data where group_id = #{groupId}")
+    List<CardsGroupData> getGroupCardsList(@Param("groupId") Long groupId);
+
+    
 }
--- rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/CardsTypeListMapper.java
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/mapper/CardsTypeListMapper.java
@@ -1,6 +1,10 @@
 package indi.eiriksgata.rulateday.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import indi.eiriksgata.rulateday.pojo.CardsGroupData;
+import indi.eiriksgata.rulateday.pojo.CardsTypeList;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 /**
  * author: create by Keith
@@ -10,5 +14,23 @@ import org.apache.ibatis.annotations.Mapper;
  **/
 @Mapper
 public interface CardsTypeListMapper {
+
+    @Insert("insert into cards_type_list (id,name,content) values (#{id},#{name},#{content})")
+    void insert(CardsTypeList cardsTypeList);
+
+    @Delete("delete from cards_type_list where id = #{id}")
+    void deleteById(@Param("id") Long id);
+
+    @Delete("delete from cards_type_list where name = #{name}")
+    void deleteByName(@Param("name") String name);
+
+    @Select("select * from cards_type_list where id =#{id}")
+    CardsTypeList selectById(@Param("id") Long id);
+
+    @Select("select * from cards_type_list where name=#{name}")
+    CardsTypeList selectByName(@Param("name") String name);
+
+    @Select("select * from cards_type_list")
+    List<CardsTypeList> selectAll();
 
 }
