3342d825108b7ce933e4e16897953ea89c2be814
Eiriksgata <2353686862@qq.com>
2021-05-12 15:23:11 +0800

    Update id type to Long

--- rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/CardsGroupData.java
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/CardsGroupData.java
@@ -1,47 +1,14 @@
 package indi.eiriksgata.rulateday.pojo;
 
 
+import lombok.Data;
+
+@Data
 public class CardsGroupData {
 
-  private long id;
-  private long groupId;
-  private long type;
-  private String value;
-
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public long getGroupId() {
-    return groupId;
-  }
-
-  public void setGroupId(long groupId) {
-    this.groupId = groupId;
-  }
-
-
-  public long getType() {
-    return type;
-  }
-
-  public void setType(long type) {
-    this.type = type;
-  }
-
-
-  public String getValue() {
-    return value;
-  }
-
-  public void setValue(String value) {
-    this.value = value;
-  }
+    private Long id;
+    private Long groupId;
+    private Long typeId;
+    private String value;
 
 }
--- rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/CardsTypeList.java
+++ rulateday-dao/src/main/java/indi/eiriksgata/rulateday/pojo/CardsTypeList.java
@@ -1,37 +1,13 @@
 package indi.eiriksgata.rulateday.pojo;
 
+import lombok.Data;
 
+@Data
 public class CardsTypeList {
 
-  private long id;
-  private String name;
-  private String content;
+    private Long id;
+    private String name;
+    private String content;
 
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
 
 }
