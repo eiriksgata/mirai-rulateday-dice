468271fe83fe47040252131035866d03a7359073
Eiriksgata <2353686862@qq.com>
2021-05-21 14:03:12 +0800

    Update character Utils function

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/CharacterUtils.java
@@ -0,0 +1,20 @@
+package indi.eiriksgata.rulateday.utlis;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.utlis
+ * date: 2021/5/21
+ **/
+public class CharacterUtils {
+
+    public static String operationSymbolProcessing(String text) {
+        text = text.replaceAll("[＋➕]", "+");
+        text = text.replaceAll("[-➖—－]]", "-");
+        text = text.replaceAll("[×xX✖]", "*");
+        text = text.replaceAll("[÷➗]", "/");
+        return text;
+    }
+
+
+}
