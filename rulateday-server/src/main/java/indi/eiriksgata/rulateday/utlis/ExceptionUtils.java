cdaf1ae91c54b0481b796c880ee606acf09b8a99
Eiriksgata <2353686862@qq.com>
2021-06-07 15:59:53 +0800

    Update exception print utils

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/ExceptionUtils.java
@@ -0,0 +1,34 @@
+package indi.eiriksgata.rulateday.utlis;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * @author: create by Keith
+ * @version: v1.0
+ * @description: indi.eiriksgata.trpgservice.utils
+ * @date:2020/5/6
+ **/
+public class ExceptionUtils {
+
+    public static String getExceptionAllInfo(Exception ex) {
+        ByteArrayOutputStream out = null;
+        PrintStream pout = null;
+        String ret = "";
+        try {
+            out = new ByteArrayOutputStream();
+            pout = new PrintStream(out);
+            ex.printStackTrace(pout);
+            ret = new String(out.toByteArray());
+            out.close();
+        } catch (Exception e) {
+            return ex.getMessage();
+        } finally {
+            if (pout != null) {
+                pout.close();
+            }
+        }
+        return ret;
+    }
+
+}
