5e95a46655f32974b08d0f9a777141c64c556262
Eiriksgata <2353686862@qq.com>
2021-06-07 16:01:27 +0800

    Add api report impl

---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/service/ApiReport.java
@@ -0,0 +1,13 @@
+package indi.eiriksgata.rulateday.service;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.service
+ * date: 2021/6/7
+ **/
+public interface ApiReport {
+
+
+    void exceptionReport(String title, String content, Long qqId);
+}
---
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/service/impl/ApiReportImpl.java
@@ -0,0 +1,41 @@
+package indi.eiriksgata.rulateday.service.impl;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import indi.eiriksgata.rulateday.DiceMessageEventHandle;
+import indi.eiriksgata.rulateday.RulatedayCore;
+import indi.eiriksgata.rulateday.service.ApiReport;
+import indi.eiriksgata.rulateday.utlis.RestUtil;
+import indi.eiriksgata.rulateday.vo.ResponseBaseVo;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+/**
+ * author: create by Keith
+ * version: v1.0
+ * description: indi.eiriksgata.rulateday.service.impl
+ * date: 2021/6/7
+ **/
+public class ApiReportImpl implements ApiReport {
+
+    private String apiUrl = ResourceBundle.getBundle("resources").getString("api.server.url");
+
+    @Override
+    public void exceptionReport(String title, String content, Long qqId) {
+        String path = "/feedback/exception";
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("qqId", qqId);
+        requestData.put("content", content);
+        requestData.put("title", title);
+        ResponseBaseVo<Object> result =
+                new Gson().fromJson(RestUtil.postForJson(apiUrl + path, new Gson().toJson(requestData)),
+                        new TypeToken<ResponseBaseVo<Object>>() {
+                        }.getType());
+        if (result.getCode() != 0) {
+            RulatedayCore.INSTANCE.getLogger().info("Rulateday exception report fail.");
+        }
+    }
+
+}
