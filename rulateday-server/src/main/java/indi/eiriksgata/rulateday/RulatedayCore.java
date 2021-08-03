650bd27618854da57a5ddd8e13f7311050b22836
Eiriksgata <2353686862@qq.com>
2021-08-03 14:46:55 +0800

    Update RulatedayCore plugin load code

--- rulateday-server/src/main/java/indi/eiriksgata/rulateday/RulatedayCore.java
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/RulatedayCore.java
@@ -1,18 +1,12 @@
 package indi.eiriksgata.rulateday;
 
 import indi.eiriksgata.rulateday.utlis.LoadDatabaseFile;
-import net.mamoe.mirai.Bot;
-import net.mamoe.mirai.console.extension.PluginComponentStorage;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
-import net.mamoe.mirai.event.EventChannel;
 import net.mamoe.mirai.event.GlobalEventChannel;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.nio.charset.Charset;
-import java.nio.file.Path;
+import java.io.IOException;
 
 public class RulatedayCore extends JavaPlugin {
 
@@ -33,77 +27,27 @@ public class RulatedayCore extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Rulateday-Dice quiting...");
+
+
     }
 
     @Override
     public void onEnable() {
+        File file = new File("");
+        //输出程序路径
+        try {
+            getLogger().info("canonical:" + file.getCanonicalPath());
+            getLogger().info("absolute:" + file.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        getLogger().info("configPath:" + getConfigFolderPath());
+        getLogger().info("dataPath:" + getDataFolderPath());
+
+        LoadDatabaseFile.init();
         getLogger().info("Rulateday-Dice is enable.Register Eventing...");
         GlobalEventChannel.INSTANCE.registerListenerHost(new DiceMessageEventHandle());
-        LoadDatabaseFile.init();
     }
 
-    @Override
-    public void onLoad(@NotNull PluginComponentStorage $this$onLoad) {
-        getLogger().info("Rulateday-Dice loading...");
-    }
-
-    @Nullable
-    @Override
-    public String getResource(@NotNull String path) {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public String getResource(@NotNull String path, @NotNull Charset charset) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public Path resolveDataPath(@NotNull Path relativePath) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public Path resolveDataPath(@NotNull String relativePath) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public File resolveDataFile(@NotNull String relativePath) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public File resolveDataFile(@NotNull Path relativePath) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public Path resolveConfigPath(@NotNull String relativePath) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public File resolveConfigFile(@NotNull Path relativePath) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public File resolveConfigFile(@NotNull String relativePath) {
-        return null;
-    }
-
-    @NotNull
-    @Override
-    public Path resolveConfigPath(@NotNull Path relativePath) {
-        return null;
-    }
 }
