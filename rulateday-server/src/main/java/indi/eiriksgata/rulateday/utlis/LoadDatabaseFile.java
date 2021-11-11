87751e902aa9f1351e9a99270cde3b2c48cc87b8
Eiriksgata <2353686862@qq.com>
2021-11-11 17:26:14 +0800

    Update init data file create

--- rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/LoadDatabaseFile.java
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/utlis/LoadDatabaseFile.java
@@ -15,51 +15,78 @@ import static org.apache.ibatis.io.Resources.getResourceAsStream;
  **/
 public class LoadDatabaseFile {
 
+    private static String path;
+
     public static void init() {
         try {
+            path = RulatedayCore.INSTANCE.getDataFolderPath().toString();
+        } catch (ExceptionInInitializerError ignored) {
+            path = "";
+        }
+        try {
+            createProjectFile();
+            createCustomTextConfigFile();
+            createImageFile();
             createDatabaseFile();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static void createProjectFile() throws IOException {
+        File mkdir = new File(path);
+        if (!mkdir.exists()) {
+            mkdir.mkdirs();
+        }
+    }
+
+    public static void createCustomTextConfigFile() throws IOException {
+        String customTextConfigFileName = "custom-text.json";
+        File file = new File( "config/indi.eiriksgata.rulateday-dice/" + customTextConfigFileName);
+        InputStream inputStream = getResourceAsStream(customTextConfigFileName);
+        if (!file.exists()) {
+            System.out.println("create custom text config file...");
+            fileOut(file, inputStream);
+        }
+    }
+
+    public static void createImageFile() throws IOException {
+        File mmImages = new File(path + "/mm-images");
+        if (!mmImages.exists()) {
+            mmImages.mkdirs();
+        }
+    }
+
+
     public static void createDatabaseFile() throws IOException {
         //读取db配置文件
         ResourceBundle dbPropertiesConfig = ResourceBundle.getBundle("db");
-
         String dbFileName = dbPropertiesConfig.getString("db.file.name");
         String dbFileVersion = dbPropertiesConfig.getString("db.file.version");
         String dbCreateName = dbFileName + dbFileVersion + ".db";
         InputStream inputStream = getResourceAsStream(dbFileName + ".db");
-        String path = RulatedayCore.INSTANCE.getDataFolderPath().toString();
-
         File file = new File(path + "/" + dbCreateName);
-        File mkdir = new File(path);
-        File mmImages = new File(path + "/mm-images");
-        if (!mkdir.exists()) {
-             mkdir.mkdirs();
-        }
-        if (!mmImages.exists()) {
-            mmImages.mkdirs();
-        }
         if (!file.exists()) {
-
-//      RulatedayCore.INSTANCE.getLogger().info("Detecting no database file, creating..");
-            OutputStream output = new FileOutputStream(file);
-            byte[] bytes = new byte[1024];
-            while (true) {
-                int readLength = inputStream.read(bytes);
-                if (readLength == -1) {
-                    break;
-                }
-                byte[] outData = new byte[readLength];
-                System.arraycopy(bytes, 0, outData, 0, readLength);
-                output.write(outData);
-            }
-            output.flush();
-            inputStream.close();
-            output.close();
+            //RulatedayCore.INSTANCE.getLogger().info("Detecting no database file, creating..");
+            fileOut(file, inputStream);
         }
+    }
+
+    public static void fileOut(File file, InputStream inputStream) throws IOException {
+        OutputStream output = new FileOutputStream(file);
+        byte[] bytes = new byte[1024];
+        while (true) {
+            int readLength = inputStream.read(bytes);
+            if (readLength == -1) {
+                break;
+            }
+            byte[] outData = new byte[readLength];
+            System.arraycopy(bytes, 0, outData, 0, readLength);
+            output.write(outData);
+        }
+        output.flush();
+        inputStream.close();
+        output.close();
     }
 
 
