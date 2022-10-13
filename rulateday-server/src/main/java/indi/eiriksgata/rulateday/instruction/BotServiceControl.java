a1127aeaea1a6aa8994bb7432c5d132a3d9f1d43
Eiriksgata <2353686862@qq.com>
2022-10-13 17:44:44 +0800

    Update #113;

--- rulateday-server/src/main/java/indi/eiriksgata/rulateday/instruction/BotServiceControl.java
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/instruction/BotServiceControl.java
@@ -1,9 +1,11 @@
 package indi.eiriksgata.rulateday.instruction;
 
 import indi.eiriksgata.dice.reply.CustomText;
+import indi.eiriksgata.rulateday.config.GlobalData;
 import indi.eiriksgata.rulateday.service.BotControlService;
 import indi.eiriksgata.rulateday.service.impl.BotControlServiceImpl;
 import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.contact.MemberPermission;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 
 /**
@@ -18,27 +20,41 @@ public class BotServiceControl {
     public final static BotControlService botControl = new BotControlServiceImpl();
 
     public boolean groupBotOff(GroupMessageEvent event) {
-        long id = Bot.getInstances().get(0).getId();
-        String regex = ".*\\[mirai:at:" + id + "].*\\.botoff";
-        String messageSource = event.getMessage().toString().replaceAll(" ", "");
-        String messageContent = event.getMessage().contentToString().replaceAll(" ", "");
-        if (messageSource.matches(regex) || messageContent.equals(".botoff")) {
-            botControl.groupDisable(event.getGroup().getId());
-            event.getGroup().sendMessage(CustomText.getText("dice.bot.off"));
-            return true;
+        if (event.getSender().getPermission().getLevel() == MemberPermission.ADMINISTRATOR.getLevel() ||
+                event.getSender().getPermission().getLevel() == MemberPermission.OWNER.getLevel() ||
+                event.getSender().getId() == Long.parseLong(GlobalData.configData.getString("master.QQ.number"))
+        ) {
+            long id = Bot.getInstances().get(0).getId();
+            String regex = ".*\\[mirai:at:" + id + "].*\\.botoff";
+            String messageSource = event.getMessage().toString().replaceAll(" ", "");
+            String messageContent = event.getMessage().contentToString().replaceAll(" ", "");
+            if (messageSource.matches(regex) || messageContent.equals(".botoff")) {
+                botControl.groupDisable(event.getGroup().getId());
+                event.getGroup().sendMessage(CustomText.getText("dice.bot.off"));
+                return true;
+            }
+        } else {
+            event.getGroup().sendMessage("需要群主或者管理员或者骰主才能关闭服务。");
         }
         return false;
     }
 
     public boolean groupBotOn(GroupMessageEvent event) {
-        long id = Bot.getInstances().get(0).getId();
-        String regex = ".*\\[mirai:at:" + id + "].*\\.boton";
-        String messageSource = event.getMessage().toString().replaceAll(" ", "");
-        String messageContent = event.getMessage().contentToString().replaceAll(" ", "");
-        if (messageSource.matches(regex) || messageContent.equals(".boton")) {
-            botControl.groupEnable(event.getGroup().getId());
-            event.getGroup().sendMessage(CustomText.getText("dice.bot.on"));
-            return true;
+        if (event.getSender().getPermission().getLevel() == MemberPermission.ADMINISTRATOR.getLevel() ||
+                event.getSender().getPermission().getLevel() == MemberPermission.OWNER.getLevel() ||
+                event.getSender().getId() == Long.parseLong(GlobalData.configData.getString("master.QQ.number"))
+        ) {
+            long id = Bot.getInstances().get(0).getId();
+            String regex = ".*\\[mirai:at:" + id + "].*\\.boton";
+            String messageSource = event.getMessage().toString().replaceAll(" ", "");
+            String messageContent = event.getMessage().contentToString().replaceAll(" ", "");
+            if (messageSource.matches(regex) || messageContent.equals(".boton")) {
+                botControl.groupEnable(event.getGroup().getId());
+                event.getGroup().sendMessage(CustomText.getText("dice.bot.on"));
+                return true;
+            }
+        } else {
+            event.getGroup().sendMessage("需要群主或者管理员或者骰主才能打开服务。");
         }
         return false;
     }
