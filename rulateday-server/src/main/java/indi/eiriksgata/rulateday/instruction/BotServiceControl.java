70c6da949a5b9f4cd61faff84657e00e753310e3
Eiriksgata <2353686862@qq.com>
2022-11-10 09:44:11 +0800

    Fix group bot enable check handler error

--- rulateday-server/src/main/java/indi/eiriksgata/rulateday/instruction/BotServiceControl.java
+++ rulateday-server/src/main/java/indi/eiriksgata/rulateday/instruction/BotServiceControl.java
@@ -1,16 +1,13 @@
 package indi.eiriksgata.rulateday.instruction;
 
-import com.alibaba.fastjson2.TypeReference;
 import indi.eiriksgata.dice.reply.CustomText;
 import indi.eiriksgata.rulateday.config.GlobalData;
+import indi.eiriksgata.rulateday.init.CacheReuseData;
 import indi.eiriksgata.rulateday.service.BotControlService;
 import indi.eiriksgata.rulateday.service.impl.BotControlServiceImpl;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.contact.MemberPermission;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-
-import java.util.List;
-
 
 /**
  * author: create by Keith
@@ -24,15 +21,11 @@ public class BotServiceControl {
     public final static BotControlService botControl = new BotControlServiceImpl();
 
     public boolean groupBotOff(GroupMessageEvent event) {
-        String prefix = isPrefixMatch(event.getMessage().contentToString());
-        if (prefix == null) {
-            return false;
-        }
         long id = Bot.getInstances().get(0).getId();
-        String regex = ".*\\[mirai:at:" + id + "].*" + prefix + "botoff";
-        String messageSource = event.getMessage().toString().replaceAll(" ", "");
-        String messageContent = event.getMessage().contentToString().replaceAll(" ", "");
-        if (messageSource.matches(regex) || messageContent.equals(prefix + "botoff")) {
+        String regex = "^(.*\\[mirai:at:" + id + "].*" + CacheReuseData.prefixMatchListRegex + "botoff|" + CacheReuseData.prefixMatchListRegex + "botoff)$";
+        String messageSource = event.getMessage().toString().trim();
+        String messageContent = event.getMessage().contentToString().trim();
+        if (messageSource.matches(regex) || messageContent.matches(regex)) {
             if (event.getSender().getPermission().getLevel() == MemberPermission.ADMINISTRATOR.getLevel() ||
                     event.getSender().getPermission().getLevel() == MemberPermission.OWNER.getLevel() ||
                     event.getSender().getId() == Long.parseLong(GlobalData.configData.getString("master.QQ.number"))
@@ -48,15 +41,11 @@ public class BotServiceControl {
     }
 
     public boolean groupBotOn(GroupMessageEvent event) {
-        String prefix = isPrefixMatch(event.getMessage().contentToString());
-        if (prefix == null) {
-            return false;
-        }
         long id = Bot.getInstances().get(0).getId();
-        String regex = ".*\\[mirai:at:" + id + "].*" + prefix + "boton";
-        String messageSource = event.getMessage().toString().replaceAll(" ", "");
-        String messageContent = event.getMessage().contentToString().replaceAll(" ", "");
-        if (messageSource.matches(regex) || messageContent.equals(prefix + "boton")) {
+        String regex = "^(.*\\[mirai:at:" + id + "].*" + CacheReuseData.prefixMatchListRegex + "boton|" + CacheReuseData.prefixMatchListRegex + "boton)$";
+        String messageSource = event.getMessage().toString().trim();
+        String messageContent = event.getMessage().contentToString().trim();
+        if (messageSource.matches(regex) || messageContent.matches(regex)) {
             if (event.getSender().getPermission().getLevel() == MemberPermission.ADMINISTRATOR.getLevel() ||
                     event.getSender().getPermission().getLevel() == MemberPermission.OWNER.getLevel() ||
                     event.getSender().getId() == Long.parseLong(GlobalData.configData.getString("master.QQ.number"))
@@ -80,24 +69,20 @@ public class BotServiceControl {
         return botControl.groupIsBlacklist(id);
     }
 
-
     /*
     指令前缀检测，如果满足指令前缀，则返回该指令前缀文本，如果不满足，则返回null
      */
     public String isPrefixMatch(String inputText) {
-        List<String> list = GlobalData.configData.getObject("instructions.prefix.list", new TypeReference<List<String>>() {
-        }.getType());
         if (inputText == null || inputText.equals("")) {
             return null;
         }
-        for (String temp : list) {
+        for (String temp : CacheReuseData.prefixMatchList) {
             if (temp.length() > inputText.length()) {
                 continue;
             }
             if (temp.equals(inputText.substring(0, temp.length()))) {
                 return temp;
             }
-
         }
         return null;
     }
